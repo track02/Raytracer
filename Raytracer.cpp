7d63ce5ba7edbce29397b937458277ca572da6de
Alex Clarke <a.clarke@runbox.com>
2016-08-29 16:04:05 +0100

    Chapter 5 (Part 1) - Surface Normals

--- Raytracer.cpp
+++ Raytracer.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-
+#include <math.h>
 
 #include "ray.h"
 
@@ -55,14 +55,42 @@
 */
 
 
-bool hit_sphere(const vec3& center, float radius, const ray& r){
+//Chapter 5 (Pt.1) - Surface Normals
+
+/*
+* Use surface normals for shading, a surface normal is a vector perpendicular to the surface
+* by convention it points away from the surface. For conveniance, normals will be of unit-length (magnitude 1)
+* For our sphere the normal is in the direction of the ray-hitpoint - the center
+*
+*
+*        o  o 
+*     o        o       
+*    o     c----p-----> p-c
+*    o          o      
+*     o        o        
+*        o  o   
+*
+* 
+* A common trick to visualise normals assuming N is a unit length-vector 
+* with each component being between -1 and 1 is to map each component to the interval 0 to 1
+* and then map x/y/z to r/g/b
+* For the normal we need the hitpoint, not just whether the ray hit the sphere or not
+* we'll assume the smallest t is the closest hitpoint
+*/
+
+
+float hit_sphere(const vec3& center, float radius, const ray& r){
   vec3 oc = r.origin() - center; //(A - C)
   float a = dot(r.direction(), r.direction()); //(B*B)
   float b = 2.0 * dot(oc, r.direction()); //2 * (B*(A-C))
   float c = dot(oc, oc) - radius*radius; //((A-C)*(A-C)) - R*R
   float discriminant = b*b - 4*a*c; //quadratic formula discriminant sqrt(b^2 - 4ac)
-  return (discriminant>0);
-
+    
+  if(discriminant < 0) //Ray hasn't hit, use -1 to indicate this
+    return -1.0; 
+  else //Discriminant >= 0, return smallest root (t) (assuming this is the closest)
+    return (-b - sqrt(discriminant)) / (2.0 * a);  //Remember quadratic formula -b (+-) sqrt(b*b - 4ac) / 2a
+  
 }
 
 
@@ -73,13 +101,23 @@ bool hit_sphere(const vec3& center, float radius, const ray& r){
 //Where t can be between 1 and 0
 
 //Chp 4 - Also checks for sphere hit
+//Chp 5 - Need to know where ray hit (t value) - to determine surface normal
 vec3 color(const ray& r){
+  
+  //Does the ray hit our centered sphere?
+  float t = hit_sphere(vec3(0,0,-1), 0.5, r);
+    
+  //Yes - determine surface normal and resulting colour vector
+  if (t > 0.0){  
+    //subtracting ray at point (t) from sphere center and normalising to create surface normal
+    //Basically a vector travelling in the opposite direction of the ray from the point the sphere is hit
+    vec3 N = unit_vector(r.point_at_parameter(t) - vec3(0,0,-1));
+    return 0.5*vec3(N.x()+1, N.y()+1, N.z()+1);
+  }
 
-  if (hit_sphere(vec3(0,0,-1), 0.5, r)) //If ray r hits a sphere at (0,0,-1) w/ Radius 5
-    return vec3(1,0,0); //Return red
-
+  //No - determine background colour
   vec3 unit_direction = unit_vector(r.direction()); //Convert the direction of the ray into a unit vector (magnitude of 1)
-  float t = 0.5*(unit_direction.y() + 1.0); //Calculate some value for t depending on rays y value
+  t = 0.5*(unit_direction.y() + 1.0); //Calculate some value for t depending on rays y value
   return (1.0-t)*vec3(1.0,1.0,1.0) + t*vec3(0.5,0.7,1.0); //Create a vector using t (color)
 }
 
