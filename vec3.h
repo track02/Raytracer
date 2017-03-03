766c422001c36e3ad071c809d3c2d5aed9bf1eab
Alex Clarke <a.clarke@runbox.com>
2017-03-03 00:04:14 +0000

    Update vec3.h

--- vec3.h
+++ vec3.h
@@ -3,9 +3,7 @@
 #include <iostream>
 #pragma once
 
-
 class vec3  {
-
     
 public:
 
@@ -25,8 +23,7 @@ public:
     inline float r() const { return e[0]; }
     inline float g() const { return e[1]; }
     inline float b() const { return e[2]; }
-    
-    
+        
     //Overload operators
     inline const vec3& operator+() const { return *this; } //Returns address of current vector
     inline vec3 operator-() const { return vec3(-e[0], -e[1], -e[2]); } //Returns a new negative vector
@@ -54,8 +51,7 @@ public:
     
     //Reflection
     inline vec3 reflect(const vec3& v, const vec3& n);
-    
-    
+        
     float e[3];
 };
 
@@ -128,7 +124,6 @@ inline vec3 cross(const vec3 &v1, const vec3 &v2) {
                 (v1.e[0]*v2.e[1] - v1.e[1]*v2.e[0]));
 }
 
-
 //Updates vector values by adding passed vector values
 inline vec3& vec3::operator+=(const vec3 &v){
     e[0]  += v.e[0];
@@ -183,4 +178,3 @@ inline vec3& vec3::operator/=(const float t) {
 inline vec3 unit_vector(vec3 v) {
     return v / v.length();
 }
-
